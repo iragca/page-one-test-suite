b2c662504cc9bb19c2556078841bf15cec863615
iragca <irag.chrisandrei@mailbox.org>
2025-03-24 22:04:59 +0800

    feat: update configuration to use environment variables for BASE_URL and MONGODB_URL

--- docker_backend/routes/config.py
+++ docker_backend/routes/config.py
@@ -1,8 +1,10 @@
+import os
+
 import requests
 from faker import Faker
 
-BASE_URL = "http://localhost:5000"
-MONGODB_URL = "mongodb://localhost:27017/"
+BASE_URL = os.getenv("BASE_URL", "http://localhost:5000")
+MONGODB_URL = os.getenv("MONGODB_URL", "mongodb://localhost:27017/pageone")
 RANDOM = Faker()
 
 print(f"{BASE_URL=}, {MONGODB_URL=}")
