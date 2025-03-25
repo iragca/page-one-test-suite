f45c392453595909273beb455462259f12b3f69e
iragca <irag.chrisandrei@mailbox.org>
2025-03-25 20:02:45 +0800

    feat: add ping functions for web app and MongoDB availability checks

--- docker_backend/test_backend.py
+++ docker_backend/test_backend.py
@@ -1,19 +1,23 @@
-import pytest
 import pymongo
-
-from tests.config import MONGODB_URL
-from tests.users import Test_Users, Test_Users_username  # noqa: F401, Ruff linter ignores F401
-from tests.signup import Test_Signup  # noqa: F401
-from tests.login import Test_Login  # noqa: F401
-from tests.index import Test_Index  # noqa: F401
+import pytest
+import requests
+from loguru import logger
 from tests.books import (  # noqa: F401
+    Test_Book_User_username,  # noqa: F401
     Test_Books,  # noqa: F401
     Test_Books_id,  # noqa: F401
     Test_Books_Isbn_isbn,  # noqa: F401
-    Test_Book_User_username  # noqa: F401
 )
-from tests.profile import Test_Profile  # noqa: F401
+from tests.config import BASE_URL, MONGODB_URL
 from tests.fallback import Test_Fallback  # noqa: F401
+from tests.index import Test_Index  # noqa: F401
+from tests.login import Test_Login  # noqa: F401
+from tests.profile import Test_Profile  # noqa: F401
+from tests.signup import Test_Signup  # noqa: F401
+from tests.users import (
+    Test_Users,
+    Test_Users_username,
+)  # noqa: F401, Ruff linter ignores F401
 
 
 @pytest.fixture(autouse=True)
@@ -31,5 +35,60 @@ def DB():
     client.close()
 
 
+def ping_webapp(host):
+    """
+    Sends a GET request to the specified host to check its availability.
+
+    Args:
+        host (str): The URL of the host to ping.
+
+    Returns:
+        bool:
+            True if the host is reachable (HTTP status code 200),
+            False otherwise.
+
+    Logs:
+        Logs an error message if the request fails or the host is unreachable.
+    """
+    try:
+        response = requests.get(host)
+        response.raise_for_status()
+        return True
+
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Could not ping {host}: {e}")
+        return False
+
+
+def ping_mongodb(host):
+    """Pings a MongoDB server to check its availability.
+
+        host (str): The hostname or URI of the MongoDB server to ping.
+
+        bool: True if the server is reachable and responds to the ping command,
+              False otherwise.
+
+    Raises:
+        pymongo.errors.ServerSelectionTimeoutError:
+            If the server is not reachable within the timeout period.
+    """
+    try:
+        client = pymongo.MongoClient(host)
+        client.admin.command("ping")
+        client.close()
+        return True
+
+    except pymongo.errors.ServerSelectionTimeoutError as e:
+        logger.error(f"Could not ping {host}: {e}")
+        return False
+
+
 if __name__ == "__main__":
-    pytest.main()
+
+    # Ping the server and MongoDB
+    if ping_webapp(BASE_URL) and ping_mongodb(MONGODB_URL):
+        pytest.main()
+
+    else:  # If the server or MongoDB is not reachable, stop the tests
+        logger.error("Stopping tests")
+        exit(1)
--- docker_backend/tests/config.py
+++ docker_backend/tests/config.py
@@ -1,11 +1,8 @@
 import os
 
-import pytest  # noqa: F401
 import requests  # noqa: F401
 from faker import Faker
 
 BASE_URL = os.getenv("BASE_URL", "http://localhost:5000")
 MONGODB_URL = os.getenv("MONGODB_URL", "mongodb://localhost:27017/pageone")
 RANDOM = Faker()
-
-print(f"{BASE_URL=}, {MONGODB_URL=}")
