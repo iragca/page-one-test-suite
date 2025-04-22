f996cc2a22a41f5a05d5d46ef2f7e42d20e2b805
iragca <irag.chrisandrei@mailbox.org>
2025-04-22 20:38:22 +0800

    test: enhance login tests to check for non-existent users and incorrect passwords

--- docker_backend/tests/login.py
+++ docker_backend/tests/login.py
@@ -16,8 +16,28 @@ class Test_Login(Login):
             "password": user["password"],
         }
 
+        non_existent_user = {
+            "username": "non_existent_user",
+            "password": "non_existent_password",
+        }
+
+        # Checking if a user is not signed up
+        response = requests.post(self.login_url, json=non_existent_user)
+        self.basic_assert(response, 404)
+
+        # Signup a user
         requests.post(self.signup_url, json=user)
 
-        response = requests.post(self.login_url, json=login_data)
+        # Wrong password
+        response = requests.post(
+            self.login_url,
+            json={
+                "username": user["username"],
+                "password": "wrong_password",
+            },
+        )
+        self.basic_assert(response, 401)
 
-        self.basic_assert(response)
+        # Correct password
+        response = requests.post(self.login_url, json=login_data)
+        self.basic_assert(response, 200)
